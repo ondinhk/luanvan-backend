5dfc83dbb51931e569516b6f1a48fccc189c862b
ODKhang <ondinhk@gmail.com>
2022-08-14 23:14:09 +0700

    create project

---
+++ src/main/java/com/odkhang/luanvan/LuanvanApplication.java
@@ -0,0 +1,13 @@
+package com.odkhang.luanvan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuanvanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuanvanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/odkhang/luanvan/LuanvanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odkhang.luanvan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuanvanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
